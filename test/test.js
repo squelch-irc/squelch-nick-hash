f84f3d0d98439162b9eba98575ebbbed29c40c33
Rahat Ahmed <rahatarmanahmed@gmail.com>
2016-02-26 18:09:53 -0600

    Add case insensitivty tests

--- test/test.js
+++ test/test.js
@@ -7,9 +7,16 @@ const chance = new Chance(0);
 describe('hashNick', () => {
     it('should return index between 1 and 16', () => {
         for(let i = 0; i < 10000; i++) {
-            var index = hashNick(chance.name());
-            assert(index >= 1, 'Index is greater than or equal to 1.')
-            assert(index <= 16, 'Index is less than or equal to 1.')
+            const nick = chance.name();
+            const index = hashNick(nick);
+            assert(index >= 1, 'Index is greater than or equal to 1.');
+            assert(index <= 16, 'Index is less than or equal to 1.');
+
+            const upperIndex = hashNick(nick.toUpperCase());
+            assert(index === upperIndex, 'Hash returns same index for upper case name');
+
+            const lowerIndex = hashNick(nick.toLowerCase());
+            assert(index === lowerIndex, 'Hash returns same index for lower case name');
         }
-    })
-})
+    });
+});
